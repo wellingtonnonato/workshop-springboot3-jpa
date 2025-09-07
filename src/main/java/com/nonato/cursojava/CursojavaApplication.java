a11b8b5bd3417ee805a7c5c9c6cc634e49d84d6b
Wellington Nonato <wellingtonnonato@gmail.com>
2025-09-07 19:13:58 -0300

    Project created

---
+++ src/main/java/com/nonato/cursojava/CursojavaApplication.java
@@ -0,0 +1,13 @@
+package com.nonato.cursojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursojavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursojavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nonato/cursojava/CursojavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nonato.cursojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursojavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
