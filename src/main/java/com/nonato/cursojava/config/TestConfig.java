55bcbd38b7e5d2079e127af42b986c4f9bfb665d
Wellington Nonato <wellingtonnonato@gmail.com>
2025-09-10 07:04:47 -0300

    Payment, one-to-one association

--- src/main/java/com/nonato/cursojava/config/TestConfig.java
+++ src/main/java/com/nonato/cursojava/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.nonato.cursojava.entities.Category;
 import com.nonato.cursojava.entities.Order;
 import com.nonato.cursojava.entities.OrderItem;
+import com.nonato.cursojava.entities.Payment;
 import com.nonato.cursojava.entities.Product;
 import com.nonato.cursojava.entities.User;
 import com.nonato.cursojava.entities.enums.OrderStatus;
@@ -77,6 +78,9 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		orderRepository.save(o1);
 
 	}
 
